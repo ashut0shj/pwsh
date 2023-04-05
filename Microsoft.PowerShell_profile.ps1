a5f6b026ab17ba2cd6534f34147515628ab37016
Ashutosh <116025276+ashut0shj@users.noreply.github.com>
2023-04-04 22:19:47 -0700

    Add files via upload

---
+++ Microsoft.PowerShell_profile.ps1
@@ -0,0 +1,29 @@
+oh-my-posh init pwsh --config "C:\Users\ashut\AppData\Local\Programs\oh-my-posh\themes\bubblesextra.omp.json" | Invoke-Expression
+
+Import-Module -Name Terminal-Icons
+
+Import-Module PSReadLine
+
+# Shows navigable menu of all options when hitting Tab
+Set-PSReadLineKeyHandler -Key Tab -Function MenuComplete
+
+# Autocompleteion for Arrow keys
+Set-PSReadLineOption -HistorySearchCursorMovesToEnd
+Set-PSReadLineKeyHandler -Key UpArrow -Function HistorySearchBackward
+Set-PSReadLineKeyHandler -Key DownArrow -Function HistorySearchForward
+
+Set-PSReadLineOption -ShowToolTips
+Set-PSReadLineOption -PredictionSource HistoryAndPlugin
+Set-PSReadLineOption -PredictionViewStyle ListView
+
+"
+Hello Ashutosh !"
+# Import the Chocolatey Profile that contains the necessary code to enable
+# tab-completions to function for `choco`.
+# Be aware that if you are missing these lines from your profile, tab completion
+# for `choco` will not function.
+# See https://ch0.co/tab-completion for details.
+$ChocolateyProfile = "$env:ChocolateyInstall\helpers\chocolateyProfile.psm1"
+if (Test-Path($ChocolateyProfile)) {
+  Import-Module "$ChocolateyProfile"
+}
